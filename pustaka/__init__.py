a880680ab7def0fb76951d68e06422c1cd61fe76
somesouls <withsomesouls@gmail.com>
2026-08-20 15:34:36 +0700

    PR-8: pindahkan pustaka_routes ke paket pustaka

---
+++ pustaka/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Paket pustaka (pustaka pengetahuan) Camerad Studio.
+
+Berisi route kelola Glosarium, Disambiguasi, dan Peta/Katalog Intent.
+Sumber data (glossary_db, disambig_db, intentmap_db, stats) berada di paket
+knowledge/. Root module lama tetap tersedia sebagai shim kompatibilitas.
+"""
